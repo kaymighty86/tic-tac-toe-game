fa73c23dd964dab69f1fb2e57c62653963b35c7c
Olukayode Adeoye Simeon <kaymighty86@gmail.com>
2025-05-12 06:40:51 +0100

    Project setup
    Project setup and other initializations

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <main>
+      <section className="main_container">
+        --COMPONENTS--
+      </section>
+      --INPUT LOG--
+    </main>
+  )
+}
+
+export default App
