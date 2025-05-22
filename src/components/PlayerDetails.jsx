0b77d07e3463cc14f251a227b3eb69fc9175c2f9
Olukayode Adeoye Simeon <kaymighty86@gmail.com>
2025-05-22 06:02:07 +0100

    Optimized the game
    - Optimized the way the app executes when changing name of players by decentralizing the state change action and only calling the game manager to effect the change when the "save" button is clicked.
    - Optimized the game to appear better on smaller screens

--- src/components/PlayerDetails.jsx
+++ src/components/PlayerDetails.jsx
@@ -6,23 +6,25 @@ import { GameManagerContext } from "../contexts/GameManagerContext.js";
 
 export default function PlayerDetails({playerData, currentTurn = false}){
     const [isEditable, setIsEditable] = useState(false);
+    const [playerName, editPlayerName] = useState(playerData.name)
 
     const {changePlayerName} = useContext(GameManagerContext);
 
-    function handleEditStateChange(){
-
-        setIsEditable(prevState => !prevState);//switch between true and false relative to what the previous state value is every time the button is clicked
+    function handlePlayerNameInput(event){
+        editPlayerName(event.target.value);
     }
 
-    function handlePlayerNameInput(event){
-        changePlayerName(playerData.id ,event.target.value);
+    function handleEditSwitch(toSave){
+        if(toSave)
+            changePlayerName(playerData.id, playerName);//inform the game manager
+        setIsEditable(prevState => !prevState);//switch between true and false relative to what the previous state value is every time the button is clicked
     }
 
     return (
         <span className={`${Styles.playerDetails} ${currentTurn? Styles.currentTurn : ""}`}>
             <p>{`[ ${playerData.symbol} ]`}</p>
-            <EditableText isEditable={isEditable} value={playerData.name} onChange={handlePlayerNameInput} className={Styles.editableText}/>
-            <button onClick={handleEditStateChange}>{isEditable? "Save" : "Edit"}</button>
+            <EditableText isEditable={isEditable} value={playerName} onChange={handlePlayerNameInput} className={Styles.editableText}/>
+            <button onClick={()=>handleEditSwitch(isEditable)}>{isEditable? "Save" : "Edit"}</button>
         </span>
     );
 }
